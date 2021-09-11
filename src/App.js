b0de19df0ff3f63bcbd313716b10477e7d07e91c
fortsharif <huseinsharif.96@gmail.com>
2021-09-11 23:46:03 +0100

    soemthing something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './navbar/NavBar'
 import Hero from './hero/Hero'
 import About from './about/About'
