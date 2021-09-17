aed09b5761de3a1b86fb73435665248de2d22d4a
fortsharif <huseinsharif.96@gmail.com>
2021-09-17 05:23:20 +0100

    bug on mobile dispalying play button, dynamic rendering of background image or video if mobile or not

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -5,11 +5,31 @@ import Introduction from './Introduction'
 import Scroller from './Scroller'
 import './hero.css'
 import rockvideo from './video.mp4'
+import rockimg from './mobileimage.JPG'
 
 
 const Hero = (props) => {
+    const [mobile, setMobile] = useState(false)
+
+    useEffect(() => {
+        if (window.matchMedia("(max-width: 480px)").matches) {
+            setMobile(true)
+        }
+    }, [])
+
+
     return <div id="home" >
-        <video autoPlay loop muted
+        {mobile ? <img src={rockimg} style={{
+            position: "absolute",
+            width: "100%",
+            left: "50%",
+            top: "50%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1",
+            backgroundSize: 'contain'
+        }}></img > : <video autoPlay loop muted
             style={{
                 position: "absolute",
                 width: "100%",
@@ -22,7 +42,8 @@ const Hero = (props) => {
                 backgroundSize: 'contain'
             }}>
             <source src={rockvideo} type="video/mp4" />
-        </video>
+        </video>}
+
         <Introduction />
         <Scroller />
 
